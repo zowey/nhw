e09a0f7efe900e2c1e334ab6c82da6108e5dd156
zorana <curkovic.zorana@gmail.com>
2014-04-05 22:43:13 +0200

    sd

--- commentStar.php
+++ commentStar.php
@@ -13,7 +13,6 @@
     <!-- Bootstrap core CSS -->
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-
     <!-- Optional theme -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 
@@ -22,6 +21,8 @@
 
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 
+    <script src="build/bootstrap-rating-input.min.js"></script>
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
 
     <!-- Just for debugging purposes. Don't actually copy this line! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
@@ -31,18 +32,24 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <script src="bootstrap-source/js/tab.js"></script>
-
     <![endif]-->
 
 </head>
 
 <body>
 
+<form id="product" action="commentStar.php" method="post">
+
+<input id="productRating" class="rating" data-max="10" data-min="0" id="some_id" name="your_awesome_parameter" type="number" />
+
+</form>
+
 <p>Comment: </p>
 
-<form id="form" action="comment.php" method="post">
+<form id="form" action="commentStar.php" method="post">
     <!-- use this input to store the clicked button value -->
     <input type='hidden' name="action" />
+    <input type='picture' name="action" />
     <textarea id="komentar"  rows="3" cols="50"></textarea>
     <input type="submit" value="submit" class="submit">
 </form>
@@ -52,12 +59,13 @@
 
 <script>
 
-  $(document).ready(function()
-    {
+ $(document).ready(function(){
+
 $( "#form" ).submit(function(e) {
     e.preventDefault();
     var com =  $( "#komentar" ).val();
-    $( "p" ).append( com );
+    $( "p" ).append( com + "</br>");
+    $( "#komentar").val("");
     var url = "index.php?comment="+com;
 
     $.get(url,function(data){
@@ -66,6 +74,18 @@ $( "#form" ).submit(function(e) {
 });
 
     });
+
+
+  $(function(){
+      $('#productRating').on('change', function(){
+
+          var pid = <?php echo $_GET["product_id"]; ?>;
+          var url = "index.php?product_id="+pid+"&rating="+$(this).val();
+
+          $.get(url,function(data){
+          });
+      });
+  });
 
 </script>
 
